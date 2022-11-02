a2a747c423008305d51b2e49a5abc90134bbb72a
evlampiev-p <pavel.evlampiev@bercut.com>
2022-11-02 09:04:13 +0300

    Homework 1.8: quiz realisation in OOP

--- src/ru/otus/homeworks/homework18/Option.java
+++ src/ru/otus/homeworks/homework18/Option.java
@@ -1,4 +1,29 @@
 package ru.otus.homeworks.homework18;
 
 public class Option {
+
+    private String[][] options = new String[][]{
+        {"cs", "java", "class", "exe"},
+        {"commit", "push", "clone", "copy"},
+        {"while", "for", "loop"}
+    };
+
+    private int[] correctAnswers = new int[]{
+            3,
+            3,
+            1
+    };
+
+    public void printOptions(int questionPosition){
+        if (questionPosition <= options.length) {
+            for (int i = 0; i < options[questionPosition].length; i++) {
+                System.out.println(i+1 + ". " + options[questionPosition][i]);
+            }
+        }
+    }
+
+    public boolean checkIfOptionCorrect(int questionPosition, int option){
+        boolean result = (option == correctAnswers[questionPosition]) ? true : false;
+        return result;
+    }
 }
--- src/ru/otus/homeworks/homework18/Quiz.java
+++ src/ru/otus/homeworks/homework18/Quiz.java
@@ -1,4 +1,17 @@
 package ru.otus.homeworks.homework18;
 
 public class Quiz {
+    public static void main(String[] args) {
+        Player player = new Player();
+        Question questions = new Question();
+        Option options = new Option();
+        player.createNewUser();
+        for (int i = 0; i < questions.numberOfQuestions(); i++) {
+            questions.printQuestion(i);
+            options.printOptions(i);
+            int chosenOption = player.getPlayerChoice();
+            player.setPlayerResult(options.checkIfOptionCorrect(i,chosenOption));
+        }
+        player.printPlayerResults();
+    }
 }
